1cb36f59145b40e95ce1cf4f8b5fd6d71577d81e
4-TUNE <61144719+4-TUNE@users.noreply.github.com>
2020-06-05 11:48:31 +0300

    Добавьте файлы проекта.

---
+++ OOP_Lab3_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace OOP_Lab3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+            string text = Console.ReadLine();
+            //text = text.Replace(" ", "");
+            string[] buff = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            int np = 0;
+            for (int i = 0; i < buff.Length; i++)
+            {
+                int check = 0;
+                for (int k = 0; k < buff[i].Length; k++)
+                {
+                    check++;
+                }
+                if ((check % 2) !=0)
+                {
+                    np++;
+                }
+            }
+
+            Console.WriteLine($"Odd words : {np}");
+
+        }
+    }
+}
